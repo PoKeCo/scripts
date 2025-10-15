e6ad06324c2687385e64b7d5e9c76ef4c9629299
pokeco32 jts <pokeco32@gmail.com>
2025-10-15 16:18:25 +0900

    change main position

--- template_ros.sh
+++ template_ros.sh
@@ -1,4 +1,40 @@
 #!/bin/bash
+function main(){
+    ## Prepare 
+    set_escape_sequence
+
+    SCRIPT_DIR=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
+    THIS_SCRIPT=$(basename ${BASH_SOURCE[0]})
+
+    ## Parse Argument
+    args=""
+    while (( "$#" > 0 ));do
+        arg=$1
+        case ${arg} in
+        --help)
+            show_help
+            exit 0
+            ;;
+        *)
+            args="${args} ${arg}"
+            ;;            
+        esac
+        shift
+    done
+
+    pushd ${SCRIPT_DIR} > /dev/null
+
+    ## Prepare for ros
+    set_ros_distro
+    install_setup_bash
+    set_jetson
+
+    ## Core
+    echo_info "WORKSPACE=${WORKSPACE}"
+    echo_note "non-parsed argument(s)=${args}"
+
+    popd > /dev/null
+}
 
 function show_help(){
     echo Usage: ${THIS_SCRIPT} [OPTION]
@@ -26,13 +62,13 @@ function echo_error(){
 
 function install_setup_bash(){
     while [[ "$(pwd)" != "/" ]];do
-	if [[ -e ./install/setup.bash ]];then
-	    WORKSPACE=$(pwd)
-	    echo_info "$(pwd)/install/setup.bash"
-	    source ./install/setup.bash
-	    return "$?"
-	fi
-	cd ..
+        if [[ -e ./install/setup.bash ]];then
+            WORKSPACE=$(pwd)
+            echo_info "$(pwd)/install/setup.bash"
+            source ./install/setup.bash
+            return "$?"
+        fi
+        cd ..
     done
     echo_error "setup.bash is not found"
     return 1
@@ -65,41 +101,40 @@ cturtle
 bOX
 EOF
     if [ "${ROS_DISTRO}" == "" ];then
-	for distro in ${ros_distro_list[@]};do
-	    if [ -e /opt/ros/${distro}/ ];then
-		ROS_DISTRO="${distro}"
-		break
-	    fi
-	done
-	echo_info "ROS_DISTRO=${ROS_DISTRO}"
+        for distro in ${ros_distro_list[@]};do
+            if [ -e /opt/ros/${distro}/ ];then
+                ROS_DISTRO="${distro}"
+                break
+            fi
+        done
+        echo_info "ROS_DISTRO=${ROS_DISTRO}"
     fi
 }
 
 function set_cyclone_dds(){
     CYCLONEDDS_FILE="/home/${USER}/cyclonedds_config.xml"
     if [ -e "${CYCLONEDDS_FILE}" ];then
-	export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-	export CYCLONEDDS_URI="file://${CYCLONEDDS_FILE}"
-	echo_info  RMW_IMPLEMENTATION=${RMW_IMPLEMENTATION}
-	echo_info  CYCLONEDDS_URI=${CYCLONEDDS_URI}
+        export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+        export CYCLONEDDS_URI="file://${CYCLONEDDS_FILE}"
+        echo_info  RMW_IMPLEMENTATION=${RMW_IMPLEMENTATION}
+        echo_info  CYCLONEDDS_URI=${CYCLONEDDS_URI}
     fi    
 }
 
 function set_jetson(){
     if [ -e "/etc/nv_tegra_release" ] ;then
-	IS_JETSON=true
-	echo jetson | sudo -S sysctl -w net.core.rmem_max=2147483647
-	echo jetson | sudo -S jetson_clocks
+        IS_JETSON=true
+        echo jetson | sudo -S sysctl -w net.core.rmem_max=2147483647
+        echo jetson | sudo -S jetson_clocks
     else
-	IS_JETSON=false
+        IS_JETSON=false
     fi
     if [[ ${IS_JETSON} == true ]];then
-	echo_info This is jetson
+        echo_info This is jetson
     else
-	echo_info This is NOT jetson
+        echo_info This is NOT jetson
     fi
 }
-
 
 function set_escape_sequence(){
     #Forground color 
@@ -121,31 +156,4 @@ function set_escape_sequence(){
     LOCATE_0_0=`printf "\e[0;0H"` # Locate cursor position to 0[raw],0[col]
 }
 
-## Prepare 
-set_escape_sequence
-
-SCRIPT_DIR=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
-THIS_SCRIPT=$(basename ${BASH_SOURCE[0]})
-
-## Parse Argument
-while (( "$#" > 0 ));do
-    arg=$1
-    case ${arg} in
-	--help)
-	    show_help
-	    exit 0
-	    ;;
-    esac
-    shift
-done
-
-pushd ${SCRIPT_DIR} > /dev/null
-
-## Prepare for ros
-set_ros_distro
-install_setup_bash
-set_cyclone_dds
-set_jetson
-
-## Main
-popd > /dev/null
+main $@
